159bd2e26b634462671959b15af636898993d10c
NamiqMy <namiqmy@code.edu.az>
2023-05-18 22:47:33 +0400

    Seasons according to month

---
+++ Season/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Season
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aylara gore fesilleri tap");
+            string? str = Console.ReadLine();
+
+            int nstr = Convert.ToInt32(str);
+
+            int ay = nstr;
+
+            string sezon = DetermineSeason(ay);
+            Console.WriteLine("Sezon: " + sezon);
+
+        }
+
+        static string DetermineSeason(int month)
+        {
+            string season;
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    season = "Qis";
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    season = "Yaz";
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    season = "Yay";
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    season = "Payiz";
+                    break;
+                default:
+                    season = "Duzgun ay daxil edin";
+                    break;
+            }
+            return season;
+        }
+    }
+}
